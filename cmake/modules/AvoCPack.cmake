e844e80796bcec71fbf98f33995269797a2c2267
thosoo <mail@thaison-nguyen.de>
2025-06-22 23:12:56 +0200

    Fix Qt library detection

--- cmake/modules/AvoCPack.cmake
+++ cmake/modules/AvoCPack.cmake
@@ -90,7 +90,7 @@ if (WIN32 AND ENABLE_DEPRECATED_INSTALL_RULES)
   # Qt                                         #
   ##############################################
   get_filename_component(QT_BIN_DIR "${QT_QMAKE_EXECUTABLE}" PATH)
-  find_path(qt_BINDIR "Qt5Core.dll" PATH ${QT_BIN_DIR})
+  find_path(qt_BINDIR Qt5Core.dll PATHS "${QT_BIN_DIR}")
   set(qt_LIBS
     Qt5Core.dll
     Qt5Gui.dll
