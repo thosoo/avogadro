81250ec604d8a5856abc5ae1d017b2c7a7269ed4
thosoo <mail@thaison-nguyen.de>
2025-06-26 18:04:37 +0200

    Make xtb optimizer stop asynchronously

---
+++ libavogadro/src/tools/xtbopttool.cpp
@@ -0,0 +1,430 @@
+#include "xtbopttool.h"
+
+#include <avogadro/glwidget.h>
+#include <avogadro/camera.h>
+#include <avogadro/molecule.h>
+#include <avogadro/atom.h>
+#include <avogadro/painter.h>
+
+#include <QtWidgets/QAction>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QVBoxLayout>
+#include <Eigen/Core>
+#include <QtCore/QMutexLocker>
+
+#include <xtb.h>
+
+namespace Avogadro {
+
+XtbOptTool::XtbOptTool(QObject *parent)
+  : Tool(parent), m_glwidget(nullptr), m_thread(new XtbOptThread),
+    m_settingsWidget(nullptr), m_running(false),
+    m_setupFailed(false), m_threadsSpinBox(nullptr)
+{
+  QAction *action = activateAction();
+  action->setIcon(QIcon(QString::fromUtf8(":/xtbopttool/autoopttool.png")));
+  action->setToolTip(tr("xTB Optimization Tool"));
+
+  connect(m_thread, SIGNAL(finished(bool)), this, SLOT(finished(bool)));
+  connect(m_thread, SIGNAL(setupDone()), this, SLOT(setupDone()));
+  connect(m_thread, SIGNAL(setupFailed()), this, SLOT(setupFailed()));
+  connect(m_thread, SIGNAL(setupSucces()), this, SLOT(setupSucces()));
+  connect(m_thread, SIGNAL(finished()), this, SLOT(threadFinished()));
+}
+
+XtbOptTool::~XtbOptTool()
+{
+  if (m_thread) {
+    m_thread->stop();
+    m_thread->wait();
+    m_thread->cleanup();
+    delete m_thread;
+  }
+  if (m_settingsWidget)
+    m_settingsWidget->deleteLater();
+}
+
+int XtbOptTool::usefulness() const
+{
+  return 10;
+}
+
+QUndoCommand* XtbOptTool::mousePressEvent(GLWidget *widget, QMouseEvent *event)
+{
+  m_glwidget = widget;
+  m_lastDraggingPosition = event->pos();
+  m_clickedAtom = widget->computeClickedAtom(event->pos());
+  return nullptr;
+}
+
+QUndoCommand* XtbOptTool::mouseReleaseEvent(GLWidget *, QMouseEvent *)
+{
+  m_clickedAtom = nullptr;
+  return nullptr;
+}
+
+QUndoCommand* XtbOptTool::mouseMoveEvent(GLWidget *widget, QMouseEvent *event)
+{
+  if (m_running && m_clickedAtom) {
+    QPoint from = m_lastDraggingPosition;
+    QPoint to = event->pos();
+    translate(widget, *m_clickedAtom->pos(), from, to);
+    m_lastDraggingPosition = to;
+  }
+  return nullptr;
+}
+
+QUndoCommand* XtbOptTool::mouseDoubleClickEvent(GLWidget *, QMouseEvent *)
+{
+  return nullptr;
+}
+
+QUndoCommand* XtbOptTool::wheelEvent(GLWidget *, QWheelEvent *)
+{
+  return nullptr;
+}
+
+void XtbOptTool::translate(GLWidget *widget, const Eigen::Vector3d &what,
+                           const QPoint &from, const QPoint &to) const
+{
+  Eigen::Vector3d fromPos = widget->camera()->unProject(from, what);
+  Eigen::Vector3d toPos = widget->camera()->unProject(to, what);
+  Eigen::Vector3d atomTranslation = toPos - fromPos;
+  if (m_clickedAtom) {
+    QMutexLocker locker(&m_thread->mutex());
+    m_clickedAtom->setPos(atomTranslation + *m_clickedAtom->pos());
+    m_clickedAtom->update();
+  }
+}
+
+bool XtbOptTool::paint(GLWidget *widget)
+{
+  if (m_running || m_setupFailed) {
+    glColor3f(1.0, 1.0, 1.0);
+    if (m_setupFailed)
+      widget->painter()->drawText(QPoint(10, 20), tr("xTB setup failed"));
+    else
+      widget->painter()->drawText(QPoint(10, 20), tr("xTB optimizing..."));
+  }
+  return true;
+}
+
+QWidget* XtbOptTool::settingsWidget()
+{
+  if (!m_settingsWidget) {
+    m_settingsWidget = new QWidget;
+
+    QLabel *label = new QLabel(tr("Steps per Update:"));
+    m_stepsSpinBox = new QSpinBox(m_settingsWidget);
+    m_stepsSpinBox->setMinimum(1);
+    m_stepsSpinBox->setMaximum(50);
+    m_stepsSpinBox->setValue(4);
+
+    QLabel *threadsLabel = new QLabel(tr("Threads:"));
+    m_threadsSpinBox = new QSpinBox(m_settingsWidget);
+    m_threadsSpinBox->setMinimum(1);
+    m_threadsSpinBox->setMaximum(64);
+    m_threadsSpinBox->setValue(1);
+
+    m_buttonStartStop = new QPushButton(tr("Start"), m_settingsWidget);
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(label);
+    layout->addWidget(m_stepsSpinBox);
+    layout->addWidget(threadsLabel);
+    layout->addWidget(m_threadsSpinBox);
+    layout->addWidget(m_buttonStartStop);
+    layout->addStretch(1);
+    m_settingsWidget->setLayout(layout);
+
+    connect(m_buttonStartStop, SIGNAL(clicked()), this, SLOT(toggle()));
+    connect(m_settingsWidget, SIGNAL(destroyed()), this, SLOT(settingsWidgetDestroyed()));
+  }
+  return m_settingsWidget;
+}
+
+void XtbOptTool::settingsWidgetDestroyed()
+{
+  m_settingsWidget = nullptr;
+}
+
+void XtbOptTool::toggle()
+{
+  if (m_running)
+    disable();
+  else
+    enable();
+}
+
+void XtbOptTool::enable()
+{
+  if (m_running)
+    return;
+
+  if (!m_glwidget || !m_glwidget->molecule()) {
+    emit message(tr("No active molecule for xTB optimization"));
+    return;
+  }
+
+  connect(m_glwidget->molecule(), SIGNAL(destroyed()), this, SLOT(abort()));
+
+  QByteArray nt = QByteArray::number(m_threadsSpinBox ? m_threadsSpinBox->value() : 1);
+  qputenv("OMP_NUM_THREADS", nt);
+
+  if (!m_thread->setup(m_glwidget->molecule(), 0, m_stepsSpinBox->value())) {
+    m_setupFailed = true;
+    emit message(tr("Failed to initialize xTB"));
+    return;
+  }
+  m_setupFailed = false;
+  m_thread->start();
+  m_running = true;
+  m_buttonStartStop->setText(tr("Stop"));
+}
+
+void XtbOptTool::abort()
+{
+  disable();
+}
+
+void XtbOptTool::disable()
+{
+  if (!m_running)
+    return;
+
+  m_thread->stop();
+  if (m_buttonStartStop)
+    m_buttonStartStop->setEnabled(false);
+}
+
+void XtbOptTool::finished(bool)
+{
+  if (m_glwidget && m_glwidget->molecule()) {
+    QMutexLocker locker(&m_thread->mutex());
+    const std::vector<double> &coords = m_thread->coords();
+    int natoms = m_thread->natoms();
+    QList<Atom *> atoms = m_glwidget->molecule()->atoms();
+    if (atoms.size() >= natoms) {
+      const double bohr2ang = 0.52917721092;
+      for (int i = 0; i < natoms; ++i) {
+        double x = coords[3 * i] * bohr2ang;
+        double y = coords[3 * i + 1] * bohr2ang;
+        double z = coords[3 * i + 2] * bohr2ang;
+        atoms[i]->setPos(Eigen::Vector3d(x, y, z));
+      }
+    }
+    m_glwidget->molecule()->update();
+    m_glwidget->update();
+  }
+}
+
+void XtbOptTool::setupDone()
+{
+  // no timer needed when running in background thread
+}
+
+void XtbOptTool::setupFailed()
+{
+  m_setupFailed = true;
+  emit message(tr("xTB initialization failed"));
+  disable();
+  if (m_glwidget)
+    m_glwidget->update();
+}
+
+void XtbOptTool::setupSucces()
+{
+  m_setupFailed = false;
+}
+
+void XtbOptTool::threadFinished()
+{
+  m_thread->cleanup();
+  m_running = false;
+  m_setupFailed = false;
+  if (m_buttonStartStop)
+    m_buttonStartStop->setText(tr("Start"));
+  if (m_buttonStartStop)
+    m_buttonStartStop->setEnabled(true);
+  if (m_glwidget)
+    m_glwidget->update();
+}
+
+XtbOptThread::XtbOptThread(QObject *parent)
+  : QThread(parent), m_molecule(nullptr), m_env(nullptr), m_xtbMol(nullptr),
+    m_calc(nullptr), m_results(nullptr), m_stop(false)
+{
+}
+
+XtbOptThread::~XtbOptThread()
+{
+  if (m_results)
+    xtb_delResults(&m_results);
+  if (m_calc)
+    xtb_delCalculator(&m_calc);
+  if (m_xtbMol)
+    xtb_delMolecule(&m_xtbMol);
+  if (m_env)
+    xtb_delEnvironment(&m_env);
+}
+
+void XtbOptThread::cleanup()
+{
+  if (m_results) {
+    xtb_delResults(&m_results);
+    m_results = nullptr;
+  }
+  if (m_calc) {
+    xtb_delCalculator(&m_calc);
+    m_calc = nullptr;
+  }
+  if (m_xtbMol) {
+    xtb_delMolecule(&m_xtbMol);
+    m_xtbMol = nullptr;
+  }
+  if (m_env) {
+    xtb_delEnvironment(&m_env);
+    m_env = nullptr;
+  }
+}
+
+bool XtbOptThread::setup(Molecule *mol, int algorithm, int steps)
+{
+  Q_UNUSED(algorithm);
+  m_mutex.lock();
+  m_molecule = mol;
+  m_steps = steps;
+  m_stop = false;
+
+  m_env = xtb_newEnvironment();
+  if (!m_env) {
+    m_mutex.unlock();
+    emit setupFailed();
+    return false;
+  }
+  int natoms = m_molecule ? m_molecule->numAtoms() : 0;
+  m_numbers.resize(natoms);
+  m_coords.resize(natoms * 3);
+  const double ang2bohr = 1.8897259886;
+  for (int i=0;i<natoms;++i) {
+    const Atom *a = m_molecule->atom(i);
+    m_numbers[i] = a->atomicNumber();
+    Eigen::Vector3d p = *a->pos();
+    m_coords[3*i] = p.x()*ang2bohr;
+    m_coords[3*i+1] = p.y()*ang2bohr;
+    m_coords[3*i+2] = p.z()*ang2bohr;
+  }
+  double charge = 0.0;
+  int uhf = 0;
+  double lattice[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+  bool periodic[3] = {false,false,false};
+  m_xtbMol = xtb_newMolecule(m_env, &natoms, m_numbers.data(), m_coords.data(),
+                             &charge, &uhf, &lattice[0][0], periodic);
+  m_calc = xtb_newCalculator();
+  if (!m_xtbMol || !m_calc) {
+    m_mutex.unlock();
+    emit setupFailed();
+    return false;
+  }
+  xtb_loadGFN2xTB(m_env, m_xtbMol, m_calc, NULL);
+  m_results = xtb_newResults();
+  if (!m_results) {
+    m_mutex.unlock();
+    emit setupFailed();
+    return false;
+  }
+  m_mutex.unlock();
+  emit setupSucces();
+  emit setupDone();
+  return true;
+}
+
+void XtbOptThread::run()
+{
+  while (!m_stop) {
+    update();
+    if (m_stop)
+      break;
+    emit finished(true);
+  }
+}
+
+void XtbOptThread::update()
+{
+  QMutexLocker locker(&m_mutex);
+  if (!m_env || !m_xtbMol || !m_calc || !m_results || !m_molecule)
+    return;
+
+  int natoms = m_numbers.size();
+  const double ang2bohr = 1.8897259886;
+  for (int i = 0; i < natoms; ++i) {
+    const Atom *a = m_molecule->atom(i);
+    Eigen::Vector3d p = *a->pos();
+    m_coords[3 * i] = p.x() * ang2bohr;
+    m_coords[3 * i + 1] = p.y() * ang2bohr;
+    m_coords[3 * i + 2] = p.z() * ang2bohr;
+  }
+
+  for (int s = 0; s < m_steps && !m_stop; ++s) {
+    xtb_updateMolecule(m_env, m_xtbMol, m_coords.data(), NULL);
+    xtb_singlepoint(m_env, m_xtbMol, m_calc, m_results);
+    std::vector<double> grad(natoms * 3);
+    xtb_getGradient(m_env, m_results, grad.data());
+    double step = 0.1;
+    for (int i = 0; i < natoms * 3; ++i)
+      m_coords[i] -= step * grad[i];
+  }
+}
+
+void XtbOptThread::stop()
+{
+  m_stop = true;
+}
+
+XtbOptCommand::XtbOptCommand(Molecule *mol, XtbOptTool *tool, QUndoCommand *parent)
+  : QUndoCommand(parent), m_moleculeCopy(*mol), m_molecule(mol), m_tool(tool)
+{
+  setText(QObject::tr("xTB Optimize"));
+}
+
+void XtbOptCommand::redo()
+{
+  m_moleculeCopy = *m_molecule;
+}
+
+void XtbOptCommand::undo()
+{
+  if (m_tool)
+    m_tool->disable();
+  *m_molecule = m_moleculeCopy;
+}
+
+bool XtbOptCommand::mergeWith(const QUndoCommand *)
+{
+  return true;
+}
+
+int XtbOptCommand::id() const
+{
+  return 1311388;
+}
+
+void XtbOptTool::writeSettings(QSettings &settings) const
+{
+  Tool::writeSettings(settings);
+  if (m_stepsSpinBox)
+    settings.setValue("steps", m_stepsSpinBox->value());
+  if (m_threadsSpinBox)
+    settings.setValue("threads", m_threadsSpinBox->value());
+}
+
+void XtbOptTool::readSettings(QSettings &settings)
+{
+  Tool::readSettings(settings);
+  if (m_stepsSpinBox)
+    m_stepsSpinBox->setValue(settings.value("steps", 4).toInt());
+  if (m_threadsSpinBox)
+    m_threadsSpinBox->setValue(settings.value("threads", 1).toInt());
+}
+
+} // namespace Avogadro
---
+++ libavogadro/tests/xtbopttooltest.cpp
@@ -0,0 +1,106 @@
+#include "config.h"
+#include <QtTest>
+
+#include <avogadro/pluginmanager.h>
+#include <avogadro/tool.h>
+#include <avogadro/molecule.h>
+#include <avogadro/atom.h>
+#include <xtb.h>
+#include <QDir>
+
+using Avogadro::PluginManager;
+using Avogadro::Tool;
+
+class XtbOptToolTest : public QObject
+{
+  Q_OBJECT
+private slots:
+  void pluginLoaded();
+  void optimizeWater();
+};
+
+void XtbOptToolTest::pluginLoaded()
+{
+  QStringList pluginDirs = PluginManager::instance()->pluginPath();
+  bool pluginExists = false;
+  foreach (const QString &dir, pluginDirs) {
+    QDir d(dir);
+    QStringList files = d.entryList(QStringList() << "*xtbopttool*", QDir::Files);
+    if (!files.isEmpty()) {
+      pluginExists = true;
+      break;
+    }
+  }
+  if (!pluginExists)
+    QSKIP("xtbopttool plugin not built");
+
+  QList<Tool *> tools = PluginManager::instance()->tools(this);
+  bool found = false;
+  foreach (Tool *tool, tools) {
+    if (tool->objectName() == QLatin1String("XtbOptimization")) {
+      found = true;
+      break;
+    }
+  }
+  QVERIFY(found);
+}
+
+void XtbOptToolTest::optimizeWater()
+{
+  Avogadro::Molecule mol;
+  Avogadro::Atom *o = mol.addAtom(8, Eigen::Vector3d(0.0, 0.0, 0.0));
+  Avogadro::Atom *h1 = mol.addAtom(1, Eigen::Vector3d(2.0, 0.0, 0.0));
+  Avogadro::Atom *h2 = mol.addAtom(1, Eigen::Vector3d(-2.0, 0.0, 0.0));
+  mol.addBond(o, h1);
+  mol.addBond(o, h2);
+
+  double initialDist = (*h1->pos() - *o->pos()).norm();
+
+  xtb_TEnvironment env = xtb_newEnvironment();
+  int natoms = mol.numAtoms();
+  std::vector<int> nums(natoms);
+  std::vector<double> coords(natoms * 3);
+  const double ang2bohr = 1.8897259886;
+  for (int i = 0; i < natoms; ++i) {
+    Avogadro::Atom *a = mol.atom(i);
+    nums[i] = a->atomicNumber();
+    Eigen::Vector3d p = *a->pos();
+    coords[3 * i] = p.x() * ang2bohr;
+    coords[3 * i + 1] = p.y() * ang2bohr;
+    coords[3 * i + 2] = p.z() * ang2bohr;
+  }
+  double charge = 0.0;
+  int uhf = 0;
+  double lattice[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+  bool periodic[3] = {false, false, false};
+  xtb_TMolecule xmol = xtb_newMolecule(env, &natoms, nums.data(), coords.data(),
+                                       &charge, &uhf, &lattice[0][0], periodic);
+  xtb_TCalculator calc = xtb_newCalculator();
+  xtb_loadGFN2xTB(env, xmol, calc, NULL);
+  xtb_TResults res = xtb_newResults();
+
+  for (int s = 0; s < 50; ++s) {
+    xtb_updateMolecule(env, xmol, coords.data(), NULL);
+    xtb_singlepoint(env, xmol, calc, res);
+    std::vector<double> grad(natoms * 3);
+    xtb_getGradient(env, res, grad.data());
+    double step = 0.1;
+    for (int i = 0; i < natoms * 3; ++i)
+      coords[i] -= step * grad[i];
+  }
+
+  xtb_delResults(&res);
+  xtb_delCalculator(&calc);
+  xtb_delMolecule(&xmol);
+  xtb_delEnvironment(&env);
+
+  const double bohr2ang = 0.52917721092;
+  double optimizedDist =
+      std::sqrt(std::pow(coords[3] * bohr2ang - coords[0] * bohr2ang, 2) +
+                std::pow(coords[4] * bohr2ang - coords[1] * bohr2ang, 2) +
+                std::pow(coords[5] * bohr2ang - coords[2] * bohr2ang, 2));
+  QVERIFY(optimizedDist < initialDist);
+}
+
+QTEST_MAIN(XtbOptToolTest)
+#include "moc_xtbopttooltest.cpp"
