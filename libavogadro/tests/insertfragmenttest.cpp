a18557626f6d7b964eb4d1570b7a2f50c3b9814c
thosoo <mail@thaison-nguyen.de>
2025-07-04 18:18:24 +0200

    Add tests for insert fragment SMILES build

---
+++ libavogadro/tests/insertfragmenttest.cpp
@@ -0,0 +1,46 @@
+#include "config.h"
+#include <QtTest>
+
+#include <openbabel/obconversion.h>
+#include <openbabel/builder.h>
+#include <openbabel/mol.h>
+
+using OpenBabel::OBConversion;
+using OpenBabel::OBMol;
+using OpenBabel::OBBuilder;
+
+class InsertFragmentTest : public QObject
+{
+  Q_OBJECT
+
+private slots:
+  void validSmilesBuild();
+  void invalidSmilesBuild();
+};
+
+void InsertFragmentTest::validSmilesBuild()
+{
+  OBConversion conv;
+  QVERIFY(conv.SetInFormat("smi"));
+  OBMol mol;
+  QVERIFY(conv.ReadString(&mol, "CCO"));
+
+  OBBuilder builder;
+  QVERIFY(builder.Build(mol));
+  QVERIFY(mol.NumAtoms() > 0);
+}
+
+void InsertFragmentTest::invalidSmilesBuild()
+{
+  OBConversion conv;
+  QVERIFY(conv.SetInFormat("smi"));
+  OBMol mol;
+  QVERIFY(!conv.ReadString(&mol, "C1C"));
+
+  OBBuilder builder;
+  QVERIFY(!builder.Build(mol));
+  QCOMPARE(mol.NumAtoms(), static_cast<unsigned int>(0));
+}
+
+QTEST_MAIN(InsertFragmentTest)
+#include "moc_insertfragmenttest.cpp"
