934c81c38e3f5cb38b0bb7c797c5b9f2942e170a
Your Name <you@example.com>
2025-08-02 12:01:07 +0200

    Set selection sphere alpha to fully opaque

--- libavogadro/src/color.cpp
+++ libavogadro/src/color.cpp
@@ -97,7 +97,7 @@ namespace Avogadro {
     m_channels[0] = 0.3;
     m_channels[1] = 0.6;
     m_channels[2] = 1.0;
-    m_channels[3] = 0.7;
+    m_channels[3] = 1.0; // fully opaque selection sphere
   }
 
   void Color::setFromQColor(const QColor &color)
