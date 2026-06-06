43f1048a87ae49ad2b06d297c169e03059348a2e
Thaison Nguyen <mail@thaison-nguyen.de>
2026-06-06 22:35:18 +0200

    Fix optional Python dependency propagation

--- cmake/modules/FindSIP.cmake
+++ cmake/modules/FindSIP.cmake
@@ -2,11 +2,10 @@
 # Find the python sip includes making sure it is loadable from python
 # This module defines
 #  SIP_INCLUDE_DIR, where to find sip.h, etc.
-#  SIP_FOUND, If false, do not try to use numpy headers.
+#  SIP_FOUND, If false, do not try to use SIP headers.
 
-# be certain sip is available from python.  this part might be unnecessary.
-execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
-  "import sip; print sip.SIP_VERSION"
+execute_process(COMMAND ${Python3_EXECUTABLE} -c
+  "import sip; print(sip.SIP_VERSION)"
   OUTPUT_VARIABLE SIP_VERSION
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 
@@ -14,28 +13,22 @@ if(SIP_VERSION)
   set(SIP_PYTHON_FOUND TRUE)
   message(STATUS "SIP version ${SIP_VERSION} found")
 else()
-  message(STATUS "SIP module not available in Python." )
+  message(STATUS "SIP module not available in Python.")
   set(SIP_PYTHON_FOUND FALSE)
 endif()
 
 if(SIP_PYTHON_FOUND)
-  # search for sip.h
   if(SIP_INCLUDE_DIR)
     set(SIP_FOUND TRUE)
-  elseif(EXISTS ${PYTHON_INCLUDE_PATH}/sip.h)
-    # The SIP headers are often packaged in the Python include directory
-    set(SIP_INCLUDE_DIR ${PYTHON_INCLUDE_PATH})
   else()
-    # Attempt to find the SIP headers in the normal system path
+    set(_sip_include_hints ${Python3_INCLUDE_DIRS})
     find_path(SIP_INCLUDE_DIR NAMES sip.h
-      PATHS
-      PATH_SUFFIXES SIP
-      )
+      HINTS ${_sip_include_hints}
+      PATH_SUFFIXES SIP)
   endif()
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(SIP DEFAULT_MSG SIP_INCLUDE_DIR)
 else()
-  # give up
   set(SIP_FOUND FALSE)
 endif()
 
--- cmake/modules/PythonDeps.cmake
+++ cmake/modules/PythonDeps.cmake
@@ -1,76 +1,58 @@
-# Find All python dependencies
-# This CMake file defines
-#   ALL_PYTHON_FOUND, if false, do not try to use python.
+# Find all Python dependencies.
+#
+# This module defines:
+#   ALL_PYTHON_FOUND       - TRUE when all optional Python integration
+#                            dependencies are available.
+#   PYTHON_SUPPORT_ENABLED - TRUE when ENABLE_PYTHON was requested and all
+#                            optional Python integration dependencies are
+#                            available.
+#
+# It intentionally uses CMake's modern Python3 package so a failed optional
+# Python probe does not leave NOTFOUND include or library paths attached to
+# targets.
 
-if(Boost_PYTHON_FOUND AND PYTHONLIBS_FOUND AND NUMPY_FOUND)
-  # In cache already
-  set(ALL_PYTHON_FOUND TRUE)
+set(ALL_PYTHON_FOUND FALSE)
+set(PYTHON_SUPPORT_ENABLED FALSE)
 
+message(STATUS "Searching for python dependencies...")
+
+# Boost Python
+message(STATUS "[1/5] Boost Python")
+# CMake's FindBoost has an option to look for additional versions.
+set(Boost_ADDITIONAL_VERSIONS "1.45" "1.44" "1.43" "1.42" "1.41" "1.40"
+  "1.40.0" "1.39" "1.39.0" "1.38" "1.38.0" "1.37" "1.37.0")
+find_package(Boost COMPONENTS python)
+if(Boost_PYTHON_FOUND)
+  message(STATUS "Boost Python found...")
 else()
-  message(STATUS "Searching for python dependencies...")
-
-  # Boost Python
-  message(STATUS "[1/5] Boost Python")
-  # CMake's new FindBoost has an option to look for additional versions
-  set(Boost_ADDITIONAL_VERSIONS "1.45" "1.44" "1.43" "1.42" "1.41" "1.40"
-    "1.40.0" "1.39" "1.39.0" "1.38" "1.38.0" "1.37" "1.37.0")
-  find_package(Boost COMPONENTS python)
-  if (Boost_PYTHON_FOUND)
-    message(STATUS "Boost Python found...")
-  else()
-    message(STATUS "Boost Python NOT found - Python support disabled.")
-  #  message(STATUS "debian/ubuntu: install the libboost-python-dev package.")
-    set(ALL_PYTHON_FOUND FALSE)
-    return()
-  endif()
-
-  # Python Libraries
-  message(STATUS "[2/5] Python Libraries")
-  find_package(PythonLibs)
-  if (NOT PYTHONLIBS_FOUND)
-    message(STATUS "Python libraries NOT found - Python support disabled.")
-  #  message(STATUS "debian/ubuntu: install the python-dev package. Correct?????")
-    set(ALL_PYTHON_FOUND FALSE)
-    return()
-  endif()
-
-  # Python Interpreter
-  message(STATUS "[3/5] Python Interpreter")
-  find_package(PythonInterp)
-  if (NOT PYTHONINTERP_FOUND)
-    message(STATUS "Python interpreter NOT found - Python support disabled.")
-  #  message(STATUS "debian/ubuntu: install the python package.")
-    set(ALL_PYTHON_FOUND FALSE)
-    return()
-  endif()
-
-  # Numpy
-  message(STATUS "[4/5] Numpy Module")
-  find_package(Numpy)
-  if (NOT NUMPY_FOUND)
-    message(STATUS "Numpy NOT found - Python support disabled.")
-  #  message(STATUS "debian/ubuntu: install the python-numpy package.")
-    set(ALL_PYTHON_FOUND FALSE)
-    return()
-  endif()
-
-  # SIP
-  message(STATUS "[5/5] SIP Module")
-  find_package(SIP)
-  if(NOT SIP_FOUND)
-    message(STATUS "sip.h header NOT found - Python support disabled")
-  #  message(STATUS "debian/ubuntu: install the python-sip4-dev package.")
-    set(ALL_PYTHON_FOUND FALSE)
-    return()
-  endif()
-
-  # Everything was found, define ENABLE_PYTHON. This can be used in code:
-  #
-  # #ifdef ENABLE_PYTHON
-  # ...
-  # #endif
-  add_definitions( -DENABLE_PYTHON )
-  # to be used in subdir CMakeLists.txt files
-  set(ALL_PYTHON_FOUND TRUE)
-
+  message(STATUS "Boost Python NOT found - Python support disabled.")
+  return()
 endif()
+
+# Python interpreter and development files
+message(STATUS "[2/5] Python3 Interpreter and Development")
+find_package(Python3 COMPONENTS Interpreter Development)
+if(NOT Python3_FOUND)
+  message(STATUS "Python3 interpreter/development files NOT found - Python support disabled.")
+  return()
+endif()
+
+# Numpy
+message(STATUS "[3/5] Numpy Module")
+find_package(Numpy)
+if(NOT NUMPY_FOUND)
+  message(STATUS "Numpy NOT found - Python support disabled.")
+  return()
+endif()
+
+# SIP
+message(STATUS "[4/5] SIP Module")
+find_package(SIP)
+if(NOT SIP_FOUND)
+  message(STATUS "sip.h header NOT found - Python support disabled")
+  return()
+endif()
+
+message(STATUS "[5/5] Python integration dependencies complete")
+set(ALL_PYTHON_FOUND TRUE)
+set(PYTHON_SUPPORT_ENABLED TRUE)
