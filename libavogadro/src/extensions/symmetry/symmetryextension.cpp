775f84956eaedb0dbfa20dc89a37089c137720cb
Thaison Nguyen <mail@thaison-nguyen.de>
2026-06-08 06:48:19 +0200

    Fix Qt6 property model API removals

--- libavogadro/src/extensions/symmetry/symmetryextension.cpp
+++ libavogadro/src/extensions/symmetry/symmetryextension.cpp
@@ -167,7 +167,7 @@ msym_thresholds_t sloppy_thresholds = {
 
     // check if we need an infinity symbol
     if (pointGroup[1] == '0')
-      pointGroup = pointGroup.replace(1,1,trUtf8("\u221e"));
+      pointGroup = pointGroup.replace(1,1,tr("∞"));
 
     // After first character, point group should subscript everything
     pointGroup.insert(1, "<sub>");
